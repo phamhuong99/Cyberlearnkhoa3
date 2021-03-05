9fd7ffa76e2b38d61b81af6fa601cd272f9a71a9
Phạm Thị Hương <phamhuong180419999@gmail.com>
2021-03-05 15:26:31 +0700

    demojss

--- cyberlearn_khoa3/src/App.js
+++ cyberlearn_khoa3/src/App.js
@@ -1,11 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import UserProfile from "./FormValidation/UserProfile/UserProfile";
+import DemoJSS from "./JSS_StyledComponentn/DemoJSS/DemoJSS";
 
 function App() {
   return (
     <div>
-      <UserProfile />
+      {/* <UserProfile /> */}
+      <DemoJSS />
     </div>
   );
 }
---
+++ cyberlearn_khoa3/src/JSS_StyledComponentn/DemoJSS/DemoJSS.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import { Button } from "../Components/Button";
+
+export default class DemoJSS extends Component {
+  render() {
+    return (
+      <div>
+        <Button className="button_style">Hello</Button>
+      </div>
+    );
+  }
+}
